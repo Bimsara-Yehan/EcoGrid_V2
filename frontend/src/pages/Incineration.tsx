86ea1cd90ba2c974063c3d5bee9f1f9685c114d4
sadhifumer <umersadhif@gmail.com>
2025-09-21 14:11:39 +0530

    feat(backend-incineration): integrate waste incineration backend with AI service

--- frontend/src/pages/Incineration.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// Import existing App component from vendor directory
-import App from '../vendor/incineration/App';
-
-export default function Incineration() {
-  return <App />;
-}
